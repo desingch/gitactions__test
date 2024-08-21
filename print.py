615813b651bb6f607e0b095afc835a5ce5ffc979
desingch <daniel.sing.chamorro@intel.com>
2024-08-20 21:36:01 -0600

    3th commit

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 import re
 
-frase = 'Tengo sueno x10'
-print(re.findall(r'\d',frase))
+# frase = 'Tengo sueno x10'
+# print(re.findall(r'\d',frase))
 
-# print('hello actions')
+print('hello actions')
